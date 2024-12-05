3ba6d46b327e048801cda474f0c184e2686b28ac
JinDevWS <jindev2024@gmail.com>
2024-12-05 13:36:46 +0900

    class형 컴포넌트 실습 1. 클래스형 컴포넌트와 상속의 이해 2. this의 이해

---
+++ class/pages/section17/17-01-class-counter/class.ts
@@ -0,0 +1,42 @@
+// 실행 안함. 연습용 파일임
+
+// // 클래스
+// class DateCustom {
+//   // 클래스 내부 변수, 메서드에는 const 나 function 이런거 안붙임.
+//   qqq = 3;
+//   getFullYear() {}
+//   getMonth() {}
+// }
+// // 클래스의 객체 생성
+// const myDate = new DateCustom();
+// console.log(myDate.getFullYear());
+// console.log(myDate.getMonth() + 1);
+
+// // 상속 실습
+// class Monster {
+//   power = 50;
+
+//   attack() {
+//     console.log("공격합니다!!");
+//   }
+// }
+
+// class SuperMonster extends Monster {
+//   run() {
+//     console.log("도망가야 돼!!");
+//   }
+
+//   // 오버라이딩
+//   attack() {
+//     console.log("슈퍼몬스터 필살기!!");
+//   }
+// }
+
+// const monster = new Monster();
+// monster.power; // 50
+// monster.attack(); // 공격합니다!!
+
+// const myMonster = new SuperMonster();
+// myMonster.power; // 50
+// myMonster.attack(); // 슈퍼몬스터 필살기!!
+// myMonster.run; // 도망가야 돼!!
---
+++ class/pages/section17/17-01-class-counter/index.tsx
@@ -0,0 +1,38 @@
+// Component 라는 클래스에 기본적으로 state, props 같은 기능들이 들어있다.
+// 그걸 상속 받아서 쓰는 방식으로 작성하면 된다.
+
+import { Component } from "react";
+
+export default class ClassCounterPage extends Component {
+  // 클래스 안의 변수와 메서드 앞에는
+  // this 가 다 생략되어 있다.
+  // 그래서 메서드 안에서 클래스 안의 변수를 쓰고 싶으면
+  // this.setState({어쩌구 저쩌구}) 이렇게 this 를 꼭 붙인다.
+
+  // state 를 따로따로 만들지 않고 애초에 이 state 라는 객체에 전부 때려넣음
+  state = {
+    count: 0,
+  };
+
+  //   onClickCountUp(): void {
+  //     console.log(this.state.count); // 에러 발생. this 의 성질을 이해할 필요가 있음
+  //     // this.setState({
+  //     //   count: 1,
+  //     // });
+  //   }
+  onClickCountUp = (): void => {
+    console.log(this.state.count); // 에러 발생. this 의 성질을 이해할 필요가 있음
+    this.setState({
+      count: 1,
+    });
+  };
+
+  render(): JSX.Element {
+    return (
+      <>
+        <div>{this.state.count}</div>
+        <button onClick={this.onClickCountUp}>카운트 올리기!!</button>
+      </>
+    );
+  }
+}
