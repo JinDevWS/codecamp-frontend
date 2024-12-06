567b443413079eaa63ae4b0ba867ea21318df0ab
JinDevWS <jindev2024@gmail.com>
2024-12-06 23:03:00 +0900

    데코레이터 실습(bbb 함수에 Board가 인자로 들어감)

---
+++ class_backend/decorator-example.ts
@@ -0,0 +1,11 @@
+function bbb(qqqqq: any) {
+  console.log("====================");
+  console.log(qqqqq);
+  console.log("====================");
+}
+
+@bbb // bbb 함수에 Board가 인자로 들어감
+class Board {
+  // number: number
+  // writer: string
+}
