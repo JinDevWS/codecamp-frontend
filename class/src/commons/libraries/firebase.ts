931f804e5c077aef444dd6e219cdb1ba1ce56276
JinDevWS <jindev2024@gmail.com>
2024-12-07 16:42:22 +0900

    firebase 연동 실습

---
+++ class/src/commons/libraries/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVFE9pq9yvOaRrdJk-A5yAxTeoFYyn8Fo",
+  authDomain: "myfirebaseinflearn.firebaseapp.com",
+  projectId: "myfirebaseinflearn",
+  storageBucket: "myfirebaseinflearn.firebasestorage.app",
+  messagingSenderId: "301101202120",
+  appId: "1:301101202120:web:9a612b50a7e0a6ff2c83fd",
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
