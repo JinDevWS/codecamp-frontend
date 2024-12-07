93e18a897552ac1fb12c49d401b3c9c17a408897
JinDevWS <jindev2024@gmail.com>
2024-12-07 21:16:35 +0900

    file 태그 못생겨서 ref를 이용해 예쁘게 꾸민 버튼 연동해주는 방법 실습

---
+++ class/pages/section19/19-02-image-ref/index.tsx
@@ -0,0 +1,56 @@
+import { gql, useMutation } from "@apollo/client";
+import { ChangeEvent, useRef, useState } from "react";
+import { IMutation } from "../../../src/commons/types/generated/types.js";
+
+const UPLOAD_FILE = gql`
+  mutation uploadFile($file: Upload!) {
+    uploadFile(file: $file) {
+      url
+    }
+  }
+`;
+
+export default function ImageUploadPage(): JSX.Element {
+  const [imageUrl, setImageUrl] = useState("");
+  const fileRef = useRef<HTMLInputElement>(null);
+
+  const [uploadFile] = useMutation<Pick<IMutation, "uploadFile">>(UPLOAD_FILE);
+
+  const onChangeFile = async (
+    event: ChangeEvent<HTMLInputElement>,
+  ): Promise<void> => {
+    // 배열로 들어오는 이유: multiple 일 때, 여러개 드래그 가능
+    // 옵셔녈 체이닝 활용
+    const file = event.target.files?.[0];
+    console.log(file);
+
+    const result = await uploadFile({
+      variables: { file },
+    });
+    console.log(result.data?.uploadFile.url);
+    setImageUrl(result.data?.uploadFile.url ?? "");
+  };
+
+  const onClickImage = (): void => {
+    // document.getElementById("")?.click(); // 리액트스럽지 않다!!
+    fileRef.current?.click();
+  };
+
+  return (
+    <>
+      <div
+        style={{ width: "100px", height: "100px", backgroundColor: "gray" }}
+        onClick={onClickImage}
+      >
+        이미지선택
+      </div>
+      <input
+        style={{ display: "none" }}
+        type="file"
+        onChange={onChangeFile}
+        ref={fileRef}
+      />
+      <img src={`http://storage.googleapis.com/${imageUrl}`} />
+    </>
+  );
+}
