64d33d0a723cd018150ad51bc6a6a81e2a354a34
JinDevWS <jindev2024@gmail.com>
2024-12-08 13:51:17 +0900

    search-keyword 실습(검색 결과 내에서 검색 키워드만 빨간색으로 표시하는 방법 실습) uuid 설치 및 활용방법 실습

---
+++ class/pages/section20/20-03-search-keyword/index.tsx
@@ -0,0 +1,88 @@
+import { useQuery, gql } from "@apollo/client";
+import { useState } from "react";
+import type {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../../src/commons/types/generated/types";
+import type { ChangeEvent, MouseEvent } from "react";
+import _ from "lodash";
+import { v4 as uuidv4 } from "uuid";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int, $search: String) {
+    fetchBoards(page: $page, search: $search) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function staticRoutingMovedPage(): JSX.Element {
+  // const [search, setSearch] = useState("");
+  const [keyword, setKeyword] = useState("");
+
+  const { data, refetch } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardsArgs
+  >(FETCH_BOARDS);
+
+  const onClickPage = (event: MouseEvent<HTMLSpanElement>): void => {
+    // 검색해서 refetch할 때, search 검색어가 refetch 에 이미 저장되어 있는 상태이므로 추가로 search 포함하지 않아도 됨
+    void refetch({ page: Number(event.currentTarget.id) });
+  };
+
+  // 500초까지 기다렸다가 500초가 지났는데도 추가 요청이 없다?
+  // 그러면 이제 리페치 요청 실행해줌
+  const getDebounce = _.debounce((value) => {
+    void refetch({ search: value, page: 1 });
+    setKeyword(value);
+  }, 500);
+
+  const onChangeSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    // setSearch(event.currentTarget.value);
+    // void refetch({ search: event.currentTarget.value, page: 1 });
+    getDebounce(event.currentTarget.value);
+  };
+
+  // const onClickSearch = (): void => {
+  //   void refetch({
+  //     search,
+  //     page: 1,
+  //   });
+  // };
+
+  return (
+    <div>
+      검색어입력: <input type="text" onChange={onChangeSearch} />
+      {/* <button onClick={onClickSearch}>검색하기</button> */}
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
+          <span style={{ margin: "10px" }}>
+            {el.title
+              .replaceAll(keyword, `@$%${keyword}@$%`)
+              .split("@$%")
+              .map((el) => (
+                <span
+                  key={uuidv4()}
+                  style={{ color: el === keyword ? "red" : "black" }}
+                >
+                  {el}
+                </span>
+              ))}
+          </span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+        </div>
+      ))}
+      {
+        // 10칸짜리 배열을 만들고 전부 1로 채우세요
+        new Array(10).fill(1).map((_, index) => (
+          <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>
+            {index + 1}
+          </span>
+        ))
+      }
+    </div>
+  );
+}
