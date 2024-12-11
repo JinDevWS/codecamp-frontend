e0a0432552c2c1d6acba9f041895ed519a0d6053
JinDevWS <jindev2024@gmail.com>
2024-12-11 17:11:04 +0900

    로컬스토리지를 이용한 로그인 실습

---
+++ class/pages/section23/23-02-login-localstorage/index.tsx
@@ -0,0 +1,72 @@
+import { gql, useMutation } from "@apollo/client";
+import type { ChangeEvent } from "react";
+import { useState } from "react";
+import type {
+  IMutation,
+  IMutationLoginUserArgs,
+} from "../../../src/commons/types/generated/types";
+import { useRecoilState } from "recoil";
+import { accessTokenState } from "../../../src/commons/stores";
+import { useRouter } from "next/router";
+
+const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      accessToken
+    }
+  }
+`;
+
+export default function LoginPage(): JSX.Element {
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginUser] = useMutation<
+    Pick<IMutation, "loginUser">,
+    IMutationLoginUserArgs
+  >(LOGIN_USER);
+
+  // 앞의 , 는 지우면 안 됨! 문법 임. (구조분해할당)
+  const [, setAccessToken] = useRecoilState(accessTokenState);
+
+  const onChangeEmail = (event: ChangeEvent<HTMLInputElement>): void => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onChangePassword = (event: ChangeEvent<HTMLInputElement>): void => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onClickLogin = async (): Promise<void> => {
+    try {
+      // 1. 로그인 뮤테이션 날려서 액세스토큰 받아오기
+      const result = await loginUser({
+        variables: { email, password },
+      });
+      const accessToken = result.data?.loginUser.accessToken;
+      console.log(accessToken);
+
+      // 받아온 액세스토큰을 글로벌스테이트에 저장하기
+      if (accessToken === undefined) {
+        alert("로그인에 실패했습니다! 다시 시도해 주세요!");
+        return;
+      }
+      setAccessToken(accessToken);
+      localStorage.setItem("accessToken", accessToken); // localStorage에 저장(보안상 취약한 방법, 비권장)
+
+      // 3. 로그인 성공 페이지로 이동하기
+      void router.push("/section23/23-02-login-localstorage-success");
+    } catch (error) {
+      if (error instanceof Error) alert(error.message);
+    }
+  };
+
+  return (
+    <>
+      이메일: <input type="text" onChange={onChangeEmail} />
+      비밀번호: <input type="password" onChange={onChangePassword} />
+      <button onClick={onClickLogin}>로그인</button>
+    </>
+  );
+}
--- class/src/components/commons/apollo/index.tsx
+++ class/src/components/commons/apollo/index.tsx
@@ -8,6 +8,7 @@ import {
 import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
 import { accessTokenState } from "../../../commons/stores";
 import { useRecoilState } from "recoil";
+import { useEffect } from "react";
 // import formDataAppendFile from "apollo-upload-client/formDataAppendFile.mjs";
 // import isExtractableFile from "apollo-upload-client/isExtractableFile.mjs";
 
@@ -19,7 +20,42 @@ interface IApolloSettingProps {
 }
 
 export default function ApolloSetting(props: IApolloSettingProps): JSX.Element {
-  const [accessToken] = useRecoilState(accessTokenState);
+  const [accessToken, setAcessToken] = useRecoilState(accessTokenState);
+
+  // localstorage is not defined 에러 발생!!
+  // Next.js의 렌더링 방식을 이해할 필요가 있다.
+  // 딥핑, 하이드레이션
+
+  // 1. 프리렌더링 예제 - process.browser 방법
+  // 너 지금 브라우저니? 물어보기
+  // if (process.browser) {
+  //   console.log("나는 지금 브라우저다!!!");
+  //   const result = localStorage.getItem("accessToken");
+  //   console.log(result);
+  //   setAcessToken(result ?? "");
+  // } else {
+  //   console.log(
+  //     "지금은 프론트엔드 서버다!!! 즉, yarn dev 해준 실행프로그램 내부이다!!",
+  //   );
+  // }
+
+  // 2. 프리렌더링 예제 - typeof window 방법
+  // window 는 브라우저에만 있으므로, window 유무파악으로 체크해주기도 함.
+  // if (typeof window !== "undefined") {
+  //   console.log("나는 지금 브라우저다!!!");
+  // } else {
+  //   console.log(
+  //     "지금은 프론트엔드 서버다!!! 즉, yarn dev 해준 실행프로그램 내부이다!!",
+  //   );
+  // }
+
+  // 3. 프리렌더링 무시 - useEffect 사용하는 방법 (일반적, 제일 깔끔)
+  useEffect(() => {
+    console.log("나는 지금 브라우저다!!");
+    const result = localStorage.getItem("accessToken");
+    console.log(result);
+    setAcessToken(result ?? "");
+  }, []);
 
   const uploadLink = createUploadLink({
     uri: "http://backend-practice.codebootcamp.co.kr/graphql", // 심화연습용
